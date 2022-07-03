678c214bdeaade751fe84ca81f67ff0b47d2d569
Andy Wu <andywurocks123@gmail.com>
2022-07-03 12:57:33 -0400

    added login and signup page

--- src/pages/account/Login.js
+++ src/pages/account/Login.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import {Link} from "react-router-dom"
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='container'>
+      <h1>Log In</h1>
+      <form>
+        <label for='email' className='form-label'>Email</label>
+        <input id='email' type='email' className='form-control'/>
+        <label for='password' className='form-label'>Password</label>
+        <input id='password' type='password' className='form-control'/>
+        <div class="form-check">
+          <label class="form-check-label" for="checkRemember">Remember me</label>
+          <input class="form-check-input" type="checkbox" id="checkRemember" />
+        </div>
+        <button type="submit" class="btn btn-primary">Log In</button>
+      </form>
+      <Link to='/signup'>Need An Account?</Link>
+    </div>
   )
 }
 
--- src/pages/account/Signup.js
+++ src/pages/account/Signup.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import {Link} from "react-router-dom"
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className='container'>
+      <h1>Register</h1>
+      <form>
+        <label for='email' className='form-label'>Email</label>
+        <input id='email' type='email' className='form-control'/>
+        <label for='password' className='form-label'>Password</label>
+        <input id='password' type='password' className='form-control'/>
+        <label for='confirmPassword' className='form-label'>Confirm Password</label>
+        <input id='confirmPassword' type='password' className='form-control'/>
+        <button type="submit" class="btn btn-primary">Create Account</button>
+      </form>
+      <Link to='/login'>Already Have an Account</Link>
+    </div>
   )
 }
 
