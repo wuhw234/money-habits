80d78130665aeead732247c6657e8ef78d7147a7
Andy Wu <andywurocks123@gmail.com>
2022-07-12 20:48:53 -0400

    added habits endpoint handling

--- src/routes/habits.js
+++ src/routes/habits.js
@@ -6,18 +6,17 @@ const tokenExtractor = require('./middleware/tokenExtractor')
 var router = express.Router();
 
 router.get('/', tokenExtractor, async (req, res) => {
-    try {
-      const habits = await Habit.findAll({
-        where: {
-          UserId: req.decodedToken.id
-        }
-      })
-      res.json(habits)
-    }
-    catch(error) {
-      return res.status(400).json({ error })
-    }
-
+  try {
+    const habits = await Habit.findAll({
+      where: {
+        UserId: req.decodedToken.id
+      }
+    })
+    res.json(habits)
+  }
+  catch(error) {
+    return res.status(400).json({ error })
+  }
 })
 
 router.post('/', tokenExtractor, async (req, res) => {
@@ -31,35 +30,39 @@ router.post('/', tokenExtractor, async (req, res) => {
 })
 
 router.patch('/', tokenExtractor, async (req, res) => {
-    try {
-        const habitId = req.query.id
-        const user = await User.findByPk(req.decodedToken.id)
-        const habit = await Habit.update({...req.body, UserId: user.id}, {
-            where: {
-                id: habitId,
-                UserId: user.id
-            }
-        })
-        res.json(habit)
-      } catch(error) {
-        return res.status(400).json({ error })
-      }
+  try {
+    if (!req.body['name'] || !req.body['value']) {
+      throw new Error('Invalid body')
+    }
+    const habitId = Number(req.query.id)
+    const user = await User.findByPk(req.decodedToken.id)
+    const habit = await Habit.findOne({where: { id: habitId, UserId: user.id }})
+    if (!habit) {
+      throw new Error('No habit found')
+    }
+
+    const updatedExpense = await habit.update({...req.body})
+    res.json(updatedExpense)
+  } catch(error) {
+    return res.status(400).json({ error })
+  }
 })
 
-router.delete('/', async (req, res) => {
-    try {
-        const habitId = req.query.id
-        const user = await User.findByPk(req.decodedToken.id)
-        const habit = await Habit.update({...req.body, UserId: user.id}, {
-            where: {
-                id: habitId,
-                UserId: user.id
-            }
-        })
-        res.json(habit)
-      } catch(error) {
-        return res.status(400).json({ error })
-      }
+router.delete('/', tokenExtractor, async (req, res) => {
+  try {
+    const habitId = Number(req.query.id)
+    const user = await User.findByPk(req.decodedToken.id)
+    const habit = await Habit.findOne({where: { id: habitId, UserId: user.id }})
+    console.log('habit found', habit)
+    if (!habit) {
+      throw new Error('No habit found')
+    }
+    const deletedHabit = await habit.destroy()
+    console.log('deleted habit', deletedHabit)
+    res.json(deletedHabit)
+  } catch(error) {
+    return res.status(400).json({ error })
+  }
 })
 
 module.exports = router
