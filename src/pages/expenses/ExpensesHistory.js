90484de5a517bb26942fbaae6ac86dc56cd7d5b0
Andy Wu <andywurocks123@gmail.com>
2022-07-19 20:49:51 -0400

    Added editing css changes

--- src/pages/expenses/ExpensesHistory.js
+++ src/pages/expenses/ExpensesHistory.js
@@ -2,15 +2,34 @@ import React, {useEffect, useState} from 'react'
 import expenseService  from './services/expenseService'
 
 const ExpensesHistory = ({expenses}) => {
-  
-  console.log(expenses)
+  const [editing, setEditing] = useState(-1)
+
+  const isEditing = (id) => editing === id ? true : false
+  const toggleEditing = (id) => {
+    if (id === editing) {
+      setEditing(-1)
+    }
+    else {
+      setEditing(id)
+    }
+  }
   return (
     <div>
+      <h2>
       Expenses
+      </h2>
       {expenses.map(expense => 
-        <>
-          <div>{expense.name} {expense.cost}</div>
-        </>
+        <div className="flex-container" id={expense.id}>
+          <input className="flex-child box-input" disabled={!isEditing(expense.id)} value={expense.name}/>
+          <input className="flex-child box-input" disabled={!isEditing(expense.id)} value={expense.cost} />
+          <button className="flex-child" onClick={() => toggleEditing(expense.id)}>
+            {isEditing(expense.id) 
+            ? 'Done'
+            : 'Edit'
+            }
+          </button>
+          <button className="flex-child">delete</button>
+        </div>
       )}
     </div>
   )
